e0edd67e33f785abd3a2ab9463ef0cfb7957a4eb
Aaral-Agrawal <aaralagrawal7@gmail.com>
2026-03-29 23:59:25 +0530

    deploy setting

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 
 const app = express();
 
-
+const PORT = process.env.PORT || 5000;
 // middleware FIRST
 app.use(cors({
   origin: ["http://localhost:3000", "http://localhost:3001"],
